35d2ab0ac9932f64c11f93ddc8eea69597c1c5e7
vmsr2 <gin.rutten@gmail.com>
2019-07-11 22:54:35 +0100

    add corrected cholesky decomposition function

--- sca_dimensionality_reduction.py
+++ sca_dimensionality_reduction.py
@@ -1,43 +1,37 @@
+## import necessary packages
 import numpy as np
 from numpy.linalg import svd
 import h5py as h5
 from sca_functions import *
 import matplotlib.pyplot as pl
 
-def reindex_data_ntk(data_ntk, ntk = [0,1,2]):
+def reindex_data(data_ntk, ntk = [0,1,2]):
     # reshapes data
     return data_ntk.transpose(ntk)
 
-def construct_mat(data_ntk):
+def construct_M(data_ntk):
     n, t, k = data_ntk.shape
+    # create matrix to store results
     M = np.zeros([n, n])
-
     for j in range(n):
         jtk = data_ntk[j]
-        # create matrix to hold result
-        mat_ntt = np.zeros([n, t**2])
+        # create matrix to hold temporary result
+        mat_nt2 = np.zeros([n, t**2])
         for i in range(n):
             itk = data_ntk[i]
             # construct TxT covariance matrix
             ai = itk.dot(jtk.T)/k
             # construct small cdiff and reshape into 1x(T^2)
             ai_diff = (ai - ai.T).reshape([1, t**2])
-            mat_ntt[i] = ai_diff
-
-        M += mat_ntt.dot(mat_ntt.T)
+            mat_nt2[i] = ai_diff
+        M += mat_nt2.dot(mat_nt2.T)
     return M
-
-
-def svd_on_M(M):
-    u, s, vh = svd(M, full_matrices=False)
-    return u, s, vh
 
 def project_data(data_ntk, u):
     n, t, k = data_ntk.shape
     n, nk = u.shape
-    proj_nk_t_k = u.T.dot(data_ntk.reshape([n, t*k])).reshape([nk, t,k])
-    return proj_nk_t_k
-
+    proj_nn_t_k = u.T.dot(data_ntk.reshape([n, t*k])).reshape([nk, t,k])
+    return proj_nn_t_k
 
 def demean_data(data_ntk):
     data_mean = np.expand_dims(data_ntk.mean(2),2)
@@ -57,39 +51,39 @@ def plot_data(data, title = ''):
 if __name__ == "__main__":
 
     ncomp = 6
+
+    # load data
     filepath = './churchland.h5'
     data_ntk = h5.File(filepath, 'r')['d']['value'][()]
     print('data dimension: {0}'.format(data_ntk.shape))
-
     filepath = './toy_for_v.h5'
     data_ntk = h5.File(filepath, 'r')['data'][()]
     print('reindexing...')
-    data_ntk = reindex_data_ntk(data_ntk, ntk = [0,1,2])
+
+
+    data_ntk = reindex_data(data_ntk, ntk = [0,1,2]) # reindex data if necessary
 
     print('removing mean')
     data_ntk_demeaned = demean_data(data_ntk)
 
     print('constructing M...')
-    M = construct_mat(data_ntk_demeaned)
-    u, s, vh = svd_on_M(M)
-
+    M = construct_M(data_ntk_demeaned)
+    u, s, vh = svd(M, full_matrices=False)
     plot_data(u, title = 'u')
 
     usvh = {}
-
     usvh['u'] = u
     usvh['s'] = s
     usvh['vh'] = vh
     usvh['ncomp'] = ncomp
 
+    proj_nn_t_k = project_data(data_ntk, u[:,:ncomp])
+    print('projected data dimension: {0}'.format(proj_nn_t_k.shape))
 
-    proj_nk_t_k = project_data(data_ntk, u[:,:ncomp])
-    print('projected data dimension: {0}'.format(proj_nk_t_k.shape))
-
-    proj_nk_t_k_demeaned = demean_data(proj_nk_t_k)
+    proj_nn_t_k_demeaned = demean_data(proj_nn_t_k)
     print('computing SC components')
-    sc = kron_factorise(proj_nk_t_k_demeaned, ntk = [0,1,2], spc_num = 5, kr_num = 5)
+    sc = kron_factorise(proj_nn_t_k_demeaned, ntk = [0,1,2], spc_num = 5, kr_num = 5)
 
-    np.save('proj_nk_t_k', proj_nk_t_k)
+    np.save('proj_nk_t_k', proj_nn_t_k)
     np.save('usvh', usvh)
     np.save('sc', sc)
---
+++ toy_data.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as pl
+from scipy.linalg import solve_lyapunov
+from numpy.linalg import inv
+
+def construct_chain(n, weight):
+    ## build the 'A' matrix of a 'chain network';
+    ## the weights decrease (bell-shape) as you go down to avoid to avoid build-up of variance down the chain (in the stochastic network)
+    A = np.zeros([n, n])
+    for i in range(n):
+        for j in range(n):
+            if i == j+1:
+                A[i,j] = 1 + weight*np.exp(np.sqrt(i/.5))
+    A -= np.eye(n)
+    return A
+
+def construct_reversible(C):
+    ## constructs the A matrix of a time-reversible system which achieves a desired spatial covariance i.e.: S = 0
+    return -0.5 * inv(C)
+
+def construct_noise(n, factor):
+    n_sub = n//2
+    ell_noise =  np.zeros([n, n]) #cholesky factor of the input noise; just a diagonal matrix of 1 (for the first subsys) and sqrt(factor) for the second subsys
+    ell_noise[:n_sub, :n_sub] = 1
+    ell_noise[-n_sub:, -n_sub:] = factor
+    ell_noise = np.sqrt(ell_noise)
+    return ell_noise
+
+def simulate_dynamics(n, k, duration, dt, sampling_dt, a, c, ell_noise):
+
+    n_bins = duration/dt
+    sample_every = sampling_dt/dt
+    accu = [] # accumulator
+
+    ell = np.linalg.cholesky(c)
+    x0 = ell @ np.rand.randn(n, k) # draw initial conditions (NxK) from the stationary distribution already
+
+    for t in range(n_bins):
+        noise = np.sqrt(dt/tau) * ell_noise * np.rand.randn(n, k) # draw the noise
+        x = x + dt/tau*a + noise
+
+        if np.mod(t, sample_every):
+            accu.append(x)
+        if t == n_bins:
+            print('finished')
+    return accu
+
+
+if __name__ == '__main__':
+
+    k = 500 # number of trials
+    n_sub = 25 # number of neurons in each sub-pop
+    n = 2 * n_sub # total number of neurons
+    dt = 1e-4 # simulation time step
+    sampling_dt = 2e-3 # spatial sampling resolution
+
+    duration = 0.5 # trial duration
+    tau = 20*1e-3 # membrane time constant
+
+    # fig = pl.figure()
+    # pl.imshow(A)
+    # pl.show()
+
+    factor = 3.0 # sequential (chain) sub-system; 0.2 is a good number, too :D
+    weight = 0.2
+    a_seq = construct_chain(n_sub, weight)
+    c_seq = solve_lyapunov(a_seq, -np.eye(n_sub))# find the controllability Gramian, ie stationary covariance matrix when the input is pure white noise -- which it will be
+
+    # reversible sub-system that matches the Gramian, but scaled to speed up the fluctuations which would otherwise be super slow
+    a_rev = factor * construct_reversible(c_seq)
+
+    z = np.zeros([n_sub, n_sub])
+    a = np.vstack([np.hstack([a_seq, z]), np.hstack([a_rev, z])])
+
+    ell_noise = construct_noise(n, factor)
+
+    # overall Gramian
+    c = solve_lyapunov(a, -ell_noise @ ell_noise.T)
+
+    print(np.all(np.linalg.eigvals(c) > 0))
+    x = simulate_dynamics(n, k, duration, dt, sampling_dt, a, c, ell_noise)
